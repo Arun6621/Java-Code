de33c1f74cd87d3c9d9b1fcc74452a2fb1909e1a
Arun Kolekar <101554613+Arun6621@users.noreply.github.com>
2022-03-28 12:12:12 +0530

    Add files via upload

---
+++ B.java
@@ -0,0 +1,13 @@
+package oops;
+
+public class B {
+	
+	int a=15;
+	int b=35;
+	
+	void main() {
+		
+		System.out.println("i am from india :");
+	}
+
+}
---
+++ Inheritance.java
@@ -0,0 +1,15 @@
+package oops;
+
+public class Inheritance {
+	
+	
+	
+	int a=20;
+	int b=30;
+	
+	void main() {
+		
+		System.out.println("I am from Maharastra:");
+	}
+
+}
