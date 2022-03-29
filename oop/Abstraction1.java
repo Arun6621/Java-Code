2e423160abc386d4b67e32b10e2aac4c0221891f
Arun Kolekar <101554613+Arun6621@users.noreply.github.com>
2022-03-29 21:32:43 +0530

    Add files via upload

---
+++ oop/Abstraction1.java
@@ -0,0 +1,19 @@
+package oop;
+
+
+public class Abstraction1 extends Abstraction {
+
+	void run()
+	{
+		
+		System.out.println("Going to School...");
+	}
+		
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      
+		Abstraction A=new Abstraction1();
+		 A.run();
+	}
+
+}
---
+++ oop/Inheritance.java
@@ -0,0 +1,13 @@
+package oop;
+
+public class Inheritance {
+	
+
+	double Area;
+	 void main(double r) {
+		  Area =(22*r*r)/7;
+	 }
+}
+	
+
+
