698989aac35ce055451919d80feac172a6aa5336
Arun Kolekar <101554613+Arun6621@users.noreply.github.com>
2022-03-29 10:40:56 +0530

    Add files via upload

---
+++ Arraylist.java
@@ -0,0 +1,40 @@
+package collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Arraylist {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		List<String> list = new ArrayList<String> ();
+
+		String str= "hi" ;
+
+		list.add("string");
+
+		list.add (str);
+
+		list.add (str+ str);
+
+		System.out.println (list.size());
+
+		System.out.println (list.contains (42));
+
+		System.out.println (list.contains ("hihi"));
+
+		list.remove ("hi");
+
+		System.out.println (list.size());
+
+		list.add("hi");
+
+		list.add(str);
+
+		System.out.println (list.size());
+	
+
+	}
+
+}
