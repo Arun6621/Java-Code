311bda7856f72a5687fb944d777aca593b1079d4
Arun Kolekar <101554613+Arun6621@users.noreply.github.com>
2022-03-29 10:47:52 +0530

    Add files via upload

---
+++ Hashset1.java
@@ -0,0 +1,24 @@
+package collection;
+
+import java.util.Iterator;
+
+import java.util. *;
+public class Hashset1 {
+		public static void main(String args[]) {
+		
+			HashSet<String> set=new HashSet ();
+
+		set.add("One");
+		set.add ("Two");
+		set.add("Three");
+		set.add("Four");
+		set.add("Five");
+		Iterator<String> i=set.iterator();		
+	
+		while(i.hasNext () )
+		{
+			System.out.println(i.next());
+		}
+	}
+
+}
